f71e49e4ca4bb7f75f58ecf644b97532f990c186
Igor-Nosatov <sinevor13@gmail.com>
2019-04-07 14:25:00 +0300

    cahnge register/login  form

--- resources/js/views/Register.vue
+++ resources/js/views/Register.vue
@@ -1,48 +1,57 @@
 <template>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card card-default">
-                        <div class="card-header">Register</div>
-                        <div class="card-body">
-                            <form>
-                                <div class="form-group row">
-                                    <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
-                                    <div class="col-md-6">
-                                        <input id="name" type="text" class="form-control" v-model="name" required autofocus>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                    <div class="col-md-6">
-                                        <input id="email" type="email" class="form-control" v-model="email" required>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                                    <div class="col-md-6">
-                                        <input id="password" type="password" class="form-control" v-model="password" required>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
-                                    <div class="col-md-6">
-                                        <input id="password-confirm" type="password" class="form-control" v-model="password_confirmation" required>
-                                    </div>
-                                </div>
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-6 offset-md-4">
-                                        <button type="submit" class="btn btn-primary" @click="handleSubmit">
-                                            Register
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+
+        <section class="banner-area organic-breadcrumb">
+          <div class="container">
+            <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+              <div class="col-first">
+                <h1>Login/Register</h1>
+                <nav class="d-flex align-items-center">
+                  <router-link :to="{ name: 'home' }" class="nav-link">Home<span class="lnr lnr-arrow-right"></span></router-link>
+                  <router-link :to="{ name: 'register' }" class="nav-link">Register<span class="lnr lnr-arrow-right"></span></router-link>
+                </nav>
+              </div>
             </div>
-        </div>
+          </div>
+        </section>
+
+        <section class="login_box_area section_gap">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="login_box_img">
+                  <img class="img-fluid" src="img/login.jpg" alt="">
+                  <div class="hover">
+                    <h4>New to our website?</h4>
+                    <p>There are advances being made in science and technology everyday, and a good example of this is the</p>
+                    <a class="primary-btn" href="registration.html">Create an Account</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="login_form_inner">
+                  <h3>Log in to enter</h3>
+                  <form class="row login_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+                    <div class="col-md-12 form-group">
+                      <input id="name" type="text" class="form-control" v-model="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'" required autofocus>
+                    </div>
+                    <div class="col-md-12 form-group">
+                      <input id="email" type="email" class="form-control" v-model="email"  name="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" required>
+                    </div>
+                    <div class="col-md-12 form-group">
+                      <input id="password" type="password" class="form-control" v-model="password" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" required>
+                    </div>
+                    <div class="col-md-12 form-group">
+                      <input id="password-confirm" type="password" class="form-control" v-model="password_confirmation" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" required>
+                    </div>
+                    <div class="col-md-12 form-group">
+                      <button type="submit" value="submit" class="primary-btn" @click="handleSubmit">Log In</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
     </template>
 
     <script>
